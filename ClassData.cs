dbf8abe79258b7474174901fca232f8596de818f
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2020-12-19 23:11:54 +0700

    Add project files.

---
+++ ClassData.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static ClientRest_20180140115_NurFajarIsmail.Program;
+
+namespace ClientRest_20180140115_NurFajarIsmail
+{
+    class ClassData
+    {
+        public void getData()
+        {
+          
+            var json = new WebClient().DownloadString("http://localhost:1907/Mahasiswa");
+            var data = JsonConvert.DeserializeObject<List<Mahasiswa>>(json);
+          
+
+            foreach (var mhs in data)
+            {
+                Console.WriteLine("Nama: " + mhs.nama);
+                Console.WriteLine("NIM: " + mhs.nim);
+                Console.WriteLine("Prodi: " + mhs.prodi);
+                Console.WriteLine("Angkatan: " + mhs.angkatan);
+            }
+            Console.ReadLine();
+        }
+    }
+}
